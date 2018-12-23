7918a7666e2bd251991c32b36c6258c2a06d543d
Nick Gamroth <thebeekeeper@gmail.com>
2018-12-23 10:27:55 -0600

    using prettytable crate to make things look a lot nicergit add .git add .

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+#[macro_use]
+extern crate prettytable;
+
 use std::env;
+use prettytable::Table;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -9,6 +13,9 @@ fn main() {
         val = val.trim_left_matches("0x").to_string();
     }
     let uval = i32::from_str_radix(&val, radix).expect(&format!("Failed to convert {} to an integer", val));
-    println!("Decimal     Hex          Octal        ASCII    Binary");
-    println!("{n:<width$}{h:#010X}   {o:#010o}   {a}        {b:#034b}", n=uval, width=12, h=uval, o=uval, a=((uval as u8) as char), b=uval);
+
+    let mut table = Table::new();
+    table.add_row(row!["Decimal", "Hex", "Octal", "ASCII", "Binary"]);
+    table.add_row(row![uval, format!("{:X}", uval), format!("{:o}", uval), ((uval as u8) as char), format!("{:b}", uval)]);
+    table.printstd();
 }
