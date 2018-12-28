1f4969757602e5cb85e697588d55dc4b27430d12
Nick Gamroth <thebeekeeper@gmail.com>
2018-12-27 20:08:44 -0600

    Adding a expression calculator

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # vc
-Command line value conversions
+Command line math utility.  
 
-First iteration is just going to convert between number systems used by programmers.  For exampple, show the different represntations for `1234`:
+Lets you convert between radixes.  For exampple, show the different represntations for `1234`:
 
 ```
-$ vc 1234
+$ vc -r 1234
 Decimal     Hex          Octal        ASCII    Binary
 1234        0x000004D2   0o00002322   Ò        0b00000000000000000000010011010010
 ```
@@ -12,7 +12,15 @@ Decimal     Hex          Octal        ASCII    Binary
 Or, you can put in a hex number:
 
 ```
-$ vc 0x1234
+$ vc -r 0x1234
 Decimal     Hex          Octal        ASCII    Binary
 4660        0x00001234   0o00011064   4        0b00000000000000000001001000110100
+```
+
+With no arguments, you can use it as a calculator.  So far this is really just the pest crate sample code running expressions from command line args:
+
+```
+# On bash at least, make sure to put expressions with * operators in quotes
+vc "2 + 9 * 10"
+2 * 9 + 10 = 28
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,85 @@
 #[macro_use]
 extern crate prettytable;
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+#[macro_use]
+extern crate lazy_static;
+
+use pest::Parser;
+use pest::iterators::{Pair, Pairs};
+use pest::prec_climber::{PrecClimber, Assoc, Operator};
 
 use std::env;
 use prettytable::Table;
 
+#[derive(Parser)]
+#[grammar = "calc.pest"]
+struct CalcParser;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        println!("ERROR!  Input value required.");
-        println!("\tValue with no prefix will be treated as a deicimal");
-        println!("\tValue with a 0x prefix will be treated as hex");
+    if args.len() < 2 {
+        println!("Not enough args");
+        println!("TODO: write an error message");
         return;
     }
-    let mut val = args[1].clone();
-    let mut radix = 10;
-    if val.contains("0x") {
-        radix = 16;
-        val = val.trim_left_matches("0x").to_string();
+    
+    if args[1] == "-r" {
+        convert_radix(args[2].clone());
     }
-    let uval = i32::from_str_radix(&val, radix).expect(&format!("Failed to convert {} to an integer", val));
+    else {
+        let stmt = args[1..].join(" ");
+        let pairs = CalcParser::parse(Rule::calculation, &stmt).unwrap();
+        let r = eval(pairs);
+        println!("{} = {}", stmt, r);
+    }
+}
+
+fn convert_radix(val: String) {
+    let mut radix = 10;
+    let mut v = val;
+    if v.contains("0x") {
+        radix = 16;
+        v = v.trim_left_matches("0x").to_string();
+    }
+    let uval = i32::from_str_radix(&v, radix).expect(&format!("Failed to convert {} to an integer", v));
 
     let mut table = Table::new();
     table.add_row(row!["Decimal", "Hex", "Octal", "ASCII", "Binary"]);
     table.add_row(row![uval, format!("{:X}", uval), format!("{:o}", uval), ((uval as u8) as char), format!("{:b}", uval)]);
     table.printstd();
 }
+
+lazy_static! {
+   static ref PREC_CLIMBER: PrecClimber<Rule> = {
+      use Rule::*;
+      use Assoc::*;
+
+      PrecClimber::new(vec![
+         Operator::new(add, Left) | Operator::new(subtract, Left),
+         Operator::new(multiply, Left) | Operator::new(divide, Left),
+         Operator::new(power, Right)
+      ])
+   };
+}
+
+fn eval(expression: Pairs<Rule>) -> f64 {
+   PREC_CLIMBER.climb(
+     expression,
+     |pair: Pair<Rule>| match pair.as_rule() {
+         Rule::num => pair.as_str().parse::<f64>().unwrap(),
+         Rule::expr => eval(pair.into_inner()),
+         _ => unreachable!(),
+      },
+      |lhs: f64, op: Pair<Rule>, rhs: f64| match op.as_rule() {
+         Rule::add      => lhs + rhs,
+         Rule::subtract => lhs - rhs,
+         Rule::multiply => lhs * rhs,
+         Rule::divide   => lhs / rhs,
+         Rule::power    => lhs.powf(rhs),
+         _ => unreachable!(),
+      },
+   )
+}
+
